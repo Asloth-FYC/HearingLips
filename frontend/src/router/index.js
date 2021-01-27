91db54ddcb5b6e1c0bd1cb32e5ee11ab3aab37c9
cholestella <923299065@qq.com>
2021-01-27 12:31:48 +0800

    删除无效测试组件

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -9,14 +8,6 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
-  },
-  {
-    path: '/hello',
-    name: 'Hello',
-    // route level code-splitting
-    // this generates a separate chunk (hello.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "hello" */ '../views/Flask.vue')
   },
   {
     path: '/login',
