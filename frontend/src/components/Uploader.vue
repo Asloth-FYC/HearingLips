c89321a8d161b7f98d926e935d94748706697fb3
cholestella <923299065@qq.com>
2021-01-29 22:13:43 +0800

    Uploader修改

--- frontend/src/components/Uploader.vue
+++ frontend/src/components/Uploader.vue
@@ -1,16 +1,77 @@
 <template>
+  <div class="uploadBox">
     <el-upload
-    class="upload-demo"
-    drag
-    multiple>
-        <i class="el-icon-upload"></i>
-        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-        <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
+        class="upload-demo"
+        ref="upload"
+        drag
+        :auto-upload="false"
+        list-type="picture"
+        :file-list="fileList"
+        action="https://jsonplaceholder.typicode.com/posts/"
+        multiple>
+      <i class="el-icon-upload"></i>
+      <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
     </el-upload>
+    <el-button class="submit" size="small"  @click="submitUpload">submit   </el-button>
+  </div>
 </template>
 
 <script>
 export default {
-    name:'Uploader'
+    name:'Uploader',
+  data(){
+    return{
+      fileList:[],
+      filename:''
+    }
+  },
+  methods:{
+    submitUpload: function () {
+      this.$refs.upload.submit()
+    },
+  },
+
 }
 </script>
+
+<style scoped>
+.uploadBox{
+  width: auto;
+  height: auto;
+  padding-left: 2%;
+  padding-right: 2%;
+  padding-top: 4%;
+  padding-bottom: 4%;
+  background-color: white;
+
+  position: fixed;
+  top:20%;
+  left: 30%;
+
+  border-color: #393939;
+  border-style: dashed;
+  border-width: 0.8px;
+
+  font-family: 'Montserrat', sans-serif;
+  font-size: 12px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.upload-demo{
+  align-self: center;
+}
+.submit{
+  margin-top: 5%;
+  width:20%;
+  font-size: 16px;
+}
+.submit:hover {
+  background-color: #ccc;
+  box-shadow: 0px 15px 20px rgba(172, 182, 178, 0.4);
+  color: #fff;
+  transform: translateY(-4px);
+}
+</style>
