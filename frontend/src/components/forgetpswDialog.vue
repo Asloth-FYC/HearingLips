52f45ffdcebee4d335106bfdef305ef4142ff378
Asloth <xingxiu.fyc@gmail.com>
2021-02-02 17:51:39 +0800

    1

--- frontend/src/components/forgetpswDialog.vue
+++ frontend/src/components/forgetpswDialog.vue
@@ -4,7 +4,8 @@
       :visible.sync="dialogVisible"
       :lock-scroll="false"
       width="800px"
-      ref="forgetpswDialog">
+      ref="forgetpswDialog"
+      center>
     <el-form :model="form" ref="form" size="medium" label-width="80px" :rules="rules">
       <el-form-item label="用户名" prop="username">
         <el-input placeholder="请输入用户名" v-model="form.username"
@@ -24,22 +25,23 @@
         </div>
       </el-form-item>
       <el-form-item label="密码" prop="inputPassword" :error="errorText">
-        <el-input placeholder="请输入密码" type="password" maxlength="8"
-                  v-model="form.inputPassword"  @input="onPasswordInput"></el-input>
+        <el-input placeholder="请输入新密码" type="password"
+                  v-model="form.inputPassword"></el-input>
       </el-form-item>
       <el-form-item label="确认" prop="confirmPassword" :error="errorText">
-        <el-input placeholder="请再次输入密码" type="password" maxlength="8"
-                  v-model="form.confirmPassword"  @input="onPasswordInput"></el-input>
-      </el-form-item>
-      <el-form-item class="text-center">
-        <el-button type="primary" @click="onConfirm" style="width: 200px">{{btnText2}}</el-button>
+        <el-input placeholder="请再次输入密码" type="password"
+                  v-model="form.confirmPassword"></el-input>
       </el-form-item>
     </el-form>
+    <span slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="onConfirm">{{btnText2}}</el-button>
+    </span>
+    
   </el-dialog>
 </template>
 
 <script>
-import dialogmixin from "@/components/dialogMixin";
+import dialogmixin from "@/assets/js/dialogMixin";
 import { apiAdminForgetMail, apiAdminForgetPassword} from "@/api/admin";
 import md5 from 'js-md5'
 
@@ -57,13 +59,6 @@ export default {
     }
   },
   methods:{
-    onPasswordInput(val) {
-      if (this.form.inputPassword !== this.form.confirmPassword) {
-        this.errorText = '该项不能为空';
-      } else {
-        this.errorText = '';
-      }
-    },
     onConfirm() {
       this.$refs.form.validate(valid => {
         if (valid) {
@@ -133,5 +128,4 @@ const rules = {
 </script>
 
 <style scoped>
-
 </style>
