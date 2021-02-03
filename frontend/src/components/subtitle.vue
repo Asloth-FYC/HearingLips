332162da1377b9cd268bdc67026f2f388dee553b
cholestella <923299065@qq.com>
2021-02-03 17:08:23 +0800

    实现点击时间轴视频跳转功能

---
+++ frontend/src/components/subtitle.vue
@@ -0,0 +1,54 @@
+<template>
+<!--  <input type="text" v-model='time' @input="toParent"></input>-->
+  <div class="block">
+    <el-timeline>
+      <el-timeline-item v-for="sentence in sentences"
+                        :timestamp="sentence.stime"
+                        placement="top">
+        <div @click="chooseTime(sentence.stime)" class="sentence">
+          <el-card >
+            <p>{{sentence.msg}}</p>
+          </el-card>
+        </div>
+      </el-timeline-item>
+    </el-timeline>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "subtitle",
+  data(){
+    return{
+      time:'',
+      sentences:[
+        {stime:'0',msg:'录屏开始'},
+        {stime:'1',msg:'xxxxxxxxxxx'},
+        {stime:'3',msg:'yyyyyyyyyyyyyyyy'},
+        {stime:'4',msg:'yyyyyyyyyyyyyyyy'}
+      ]
+    }
+  },
+  watch:{
+    time:function (){
+      this.toParent()
+    }
+  },
+  methods:{
+    toParent() {
+      this.$emit('settime', this.time)
+    },
+    chooseTime(time){
+      console.log(time);
+      this.time=time
+    }
+  }
+}
+</script>
+
+<style scoped>
+.sentence{
+  display: block;
+}
+</style>
